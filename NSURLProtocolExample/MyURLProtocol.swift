7cf85bbc8906b706b6db7ba435ea1f7c8673d059
andrew54068 <andrew0424718012@yahoo.com.tw>
2019-06-03 16:04:23 +0800

    [feat] handle request in custom URLProtocol.

--- NSURLProtocolExample/MyURLProtocol.swift
+++ NSURLProtocolExample/MyURLProtocol.swift
@@ -12,10 +12,31 @@ var requestCount: Int = 0
 
 class MyURLProtocol: URLProtocol {
     
+    var connection: NSURLConnection!
+    
     override class func canInit(with request: URLRequest) -> Bool {
         print("Request #\(requestCount): URL = \(String(describing: request.url?.absoluteString))")
         requestCount += 1
-        return false
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override class func requestIsCacheEquivalent(_ a: URLRequest, to b: URLRequest) -> Bool {
+        return super.requestIsCacheEquivalent(a, to: b)
+    }
+    
+    override func startLoading() {
+        self.connection = NSURLConnection(request: self.request, delegate: self)
+    }
+    
+    override func stopLoading() {
+        if self.connection != nil {
+            self.connection.cancel()
+        }
+        self.connection = nil
     }
     
 }
